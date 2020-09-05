80bf1cb145e2d063c27a041a84fbca17a0b82a49
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-09-05 13:51:48 -0500

    added function to handle button clicks.

--- src/javascripts/components/quadrants.js
+++ src/javascripts/components/quadrants.js
@@ -1,15 +1,22 @@
-import quadrantData from '../../data/quadrantData';
+import QuadData from '../../data/quadrantData';
 
 const loadQuadrants = () => {
-  quadrantData.getQuadrants().forEach((quad) => $(`#${quad.quadName}`).html(`
+  QuadData.getQuadrants().forEach((quad) => $(`#${quad.quadName}`).html(`
   <div id = card-${quad.quadName} class = card-${quad.quadName} >
     <h2 class = quad-name>${quad.quadName}</h2>
     <h3 class = quad-status>${quad.statusValue}%</h2>
     <div class = button-container>
-      <button type = "button">${quad.actionValues[0].actionName}</button>
-      <button type = "button">${quad.actionValues[1].actionName}</button>
+      <button id = ${quad.actionValues[0].actionName} type = "button">${quad.actionValues[0].actionName}</button>
+      <button id = ${quad.actionValues[1].actionName} type = "button">${quad.actionValues[1].actionName}</button>
     </div>
   </div>`));
 };
 
-export default { loadQuadrants };
+const btnEventFunction = () => {
+  $('button').click((e) => {
+    const target = e.target.id;
+    console.log(target);
+  });
+};
+
+export default { loadQuadrants, btnEventFunction };
