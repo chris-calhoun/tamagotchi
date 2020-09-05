648f5877c6c59261daedae796121046c3a3522aa
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-09-05 13:20:11 -0500

    more scss styling.

--- src/javascripts/components/quadrants.js
+++ src/javascripts/components/quadrants.js
@@ -5,8 +5,10 @@ const loadQuadrants = () => {
   <div id = card-${quad.quadName} class = card-${quad.quadName} >
     <h2 class = quad-name>${quad.quadName}</h2>
     <h3 class = quad-status>${quad.statusValue}%</h2>
-    <button type = "button">${quad.actionValues[0].actionName}</button>
-    <button type = "button">${quad.actionValues[1].actionName}</button>
+    <div class = button-container>
+      <button type = "button">${quad.actionValues[0].actionName}</button>
+      <button type = "button">${quad.actionValues[1].actionName}</button>
+    </div>
   </div>`));
 };
 
